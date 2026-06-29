2ca4b3cfa046fce13f955c06aa89b2c542b5e7c5
claudiu <cperdevara@gmail.com>
2026-06-29 13:25:09 +0300

    Reviewed the requested changes

--- calculator/compile_calc.sh
+++ calculator/compile_calc.sh
@@ -6,18 +6,18 @@ BASE_DIR="src/main/java"
 PACKAGE_DIR="Calculator"
 echo "Compilation is starting"
 
-javac $BASE_DIR/$PACKAGE_DIR/Calc.java
+javac "${BASE_DIR}/${PACKAGE_DIR}"/Calc.java
 
 if [ $? -eq 0 ]; then
   echo "Compilation succesful! .class files created."
   echo "Creating the JAR file..."
   # c - create, f - specifica numele arhivei, e - entry pointul adica unde se afla functia main de unde sa inceapa
   # -C change dir
-  jar cfe $JAR_NAME $MAIN_CLASS -C $BASE_DIR $PACKAGE_DIR
+  jar cfe ${JAR_NAME} ${MAIN_CLASS} -C "${BASE_DIR}" "${PACKAGE_DIR}"
 
   if [ $? -eq 0 ]; then
     echo "The exec file has been created."
-    echo "Run: \" java -jar $JAR_NAME\""
+    echo "Run: \" java -jar ${JAR_NAME}\""
   else
     echo "Error creating the JAR file!"
     exit 1
